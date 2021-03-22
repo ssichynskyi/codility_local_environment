9f77b189c8c709c696e3def113cf0d99e4e8b86c
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-03-23 00:53:31 +0100

    add base test case for inheritance

---
+++ base/testcase.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from ast import literal_eval as parse
+
+
+class CodilityTestCase(TestCase):
+    def extract_test_data(self, test_input_file, exp_res_file):
+        with open(test_input_file) as test_data_file:
+            self.test_data = [parse(data_str) for data_str in test_data_file.readlines()]
+
+        with open(exp_res_file) as expected_results_file:
+            self.expected_results = [parse(exp_res) for exp_res in expected_results_file.readlines()]
+
+        if len(self.test_data) != len(self.expected_results):
+            raise ValueError(
+                f'Number of data lines in files {test_input_file} and {exp_res_file} shall match'
+            )
